bfaff979fda1fc2f895128800cc51a9dfd702c0b
jfiss11 <jfiss11@student.aau.dk>
2014-04-25 13:12:35 +0200

    Added weapon script to newest cleaner version

---
+++ Unity/Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	// Use this for initialization
+	public int pizzaAmmount;
+	public GameObject pizza;
+	GameObject player;
+
+	void Start () {
+		pizzaAmmount = 5;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")){
+			if (pizzaAmmount > 0){
+				GameObject clone = Instantiate(pizza, transform.position, transform.rotation) as GameObject;
+			pizzaAmmount--;
+			print(pizzaAmmount);
+			}
+		}
+	
+	}
+}
