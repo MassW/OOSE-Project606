a7d242ca689ad85dd8fefe57afcf1c7102af45c5
jfiss11 <jfiss11@student.aau.dk>
2014-04-25 14:09:21 +0200

    Retry to sync master with Weapon updates

---
+++ Unity/Assets/Scripts/Weapon/pizzaProjectile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pizzaProjectile : MonoBehaviour {
+
+	// the variable that handles whether the pizza has hit something or not
+	private bool pizzaState;
+	private GameObject playerData;
+
+	void Start(){
+		playerData = GameObject.Find("Player");// GetComponent(GameObject.FindGameObjectWithTag("Player"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (playerData.transform.localScale.x>0){
+			for(int i = 0; i>20; i++){
+
+			}
+		}
+	
+	}
+}
