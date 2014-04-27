477c426536d3d757d82500745506606374ab0dfb
jfiss11 <jfiss11@student.aau.dk>
2014-04-28 00:37:47 +0200

    Boss that throw bombs constantly

---
+++ Unity/Assets/Scripts/enemy/boss.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boss : MonoBehaviour {
+	
+	// Use this for initialization
+
+	public GameObject croissaint;
+	GameObject clone;
+	Vector3 croissantVelocity;
+	bool bossAlive;
+	
+	
+	void Start () {
+		bossAlive = true;
+
+		InvokeRepeating ("LaunchProjectile", 3,2);
+
+	}
+	void LaunchProjectile (){
+		clone = Instantiate(croissaint, transform.position, transform.rotation) as GameObject;
+		print("croissant fired");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+			
+		
+	}
+	
+}
---
+++ Unity/Assets/Scripts/enemy/croissantBomb.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class croissantBomb : MonoBehaviour {
+	PlayerHealth playerHealth;
+	// Use this for initialization
+	void Start () {
+		playerHealth = GameObject.Find("NewPlayer").gameObject.GetComponent<PlayerHealth>();
+		Destroy (gameObject,3);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(-0.1f,0.002f,0);
+
+	}
+	void OnCollisionEnter2D(Collision2D collision){
+		if(collision.gameObject.tag == "Player"){
+			PlayerHealth.curHealth -= 10;
+			Destroy(this.gameObject);
+		}else{
+			Destroy(this.gameObject);
+		}
+	}
+}
