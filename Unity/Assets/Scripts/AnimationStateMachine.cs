2ed08cab121f60bb198b4edd6957eb756db328b6
jfiss11 <jfiss11@student.aau.dk>
2014-04-27 22:17:03 +0200

    Finite State Machine for player animation

---
+++ Unity/Assets/Scripts/AnimationStateMachine.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationStateMachine : MonoBehaviour {
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis ("Horizontal");
+		print (move);
+		if (move < -0.01f || move > 0.01f)
+			animator.SetBool("move",true);
+			else	
+			animator.SetBool("move",false);
+	}
+}
