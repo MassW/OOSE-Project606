1fc011482acac67d00e94363f80f3ec0d79a1ab0
jfiss11 <jfiss11@student.aau.dk>
2014-04-27 16:05:38 +0200

    projectile direction fixed

--- Unity/Assets/Scripts/Weapon/pizzaProjectile.cs
+++ Unity/Assets/Scripts/Weapon/pizzaProjectile.cs
@@ -5,17 +5,25 @@ public class pizzaProjectile : MonoBehaviour {
 
 	// the variable that handles whether the pizza has hit something or not
 
-	public Vector3 projectileSpeed;
-	 
+
+	Controller_improved controller_improved;
+	float projectileSpeed;
 
 	void Start(){
+
+		controller_improved = GameObject.Find("NewPlayer").gameObject.GetComponent<Controller_improved>();
+
+		if(controller_improved.facingRight)
+			projectileSpeed = 0.3f;
+		else
+			projectileSpeed = -0.3f;
 
 		Destroy (gameObject,2);
 	}
 	
 	// Update is called once per frame
 	void Update (){
-		transform.Translate(0.3f,0,0);
+		transform.Translate(projectileSpeed,0,0);
 
 	}
 	void OnCollisionEnter2D(Collision2D collision){
