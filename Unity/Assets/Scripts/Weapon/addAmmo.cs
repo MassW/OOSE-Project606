956e73c429b8e82e364aba90ed6f0385449d3541
jfiss11 <jfiss11@student.aau.dk>
2014-04-27 15:16:40 +0200

    Weapon crates add pizzas and destroy

---
+++ Unity/Assets/Scripts/Weapon/addAmmo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addAmmo : MonoBehaviour {
+
+	// Use this for initialization
+	private int ammoInBox;
+	Weapon weapon;
+
+	void Start () {
+
+		ammoInBox = Random.Range(1,5);
+		print ("ammo in box " + ammoInBox);
+		weapon = GameObject.Find("NewPlayer").gameObject.GetComponent<Weapon>();
+	}
+
+	void Update(){
+
+	
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision){
+		if(collision.transform.tag == "Player"){
+			print ("collide");
+			weapon.pizzaAmmount+=ammoInBox;
+			Destroy(this.gameObject);
+
+		}
+	}
+}
