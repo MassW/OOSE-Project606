e076a90def58fd64279f50fdddc49d94282b0a3f
MassW <mwitt11@student.aau.dk>
2014-04-28 09:58:43 +0200

    Added Level element/design/scripts

---
+++ Unity/Assets/Scripts/killfloor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killfloor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+		void OnTriggerEnter2D (Collider2D col) 
+		{
+			// If it hits an enemy...
+			if(col.tag == "Player")
+			{
+				// ... find the Enemy script and call the Hurt function.
+				col.gameObject.GetComponent<PlayerHealth>().killed();
+		
+			}
+	
+	
+	
+		}
+}
---
+++ Unity/Assets/Scripts/platform_UD.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class platform_UD : MonoBehaviour {
+	
+	public List<GameObject> wayPoints;
+	public int nextWayPoint = 0;
+	public float speed = 10.0f;
+	public string state = "move";
+
+	
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+		
+		switch(state){
+		case "move": // lets enemy partol between two waypoints in the environment
+			if (wayPoints.Count > 0){
+				//transform.lookAt(wayPoints[nextWayPoint].transform.position);
+				transform.position += transform.forward * speed * Time.deltaTime;
+				
+				if (Vector3.Distance(transform.position,wayPoints[nextWayPoint].transform.position) < 0.5){
+					nextWayPoint = (nextWayPoint+1) % wayPoints.Count;
+				}
+			}
+			
+			break;
+		}
+	}
+}
