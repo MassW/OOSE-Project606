c66f721db0643c6c408e31b5ca2daa3ae206b3ab
MassW <mwitt11@student.aau.dk>
2014-04-28 17:25:26 +0200

    added fixes from LevelFixes branch

---
+++ Unity/Assets/Scripts/Move.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+	public float bounceSpeed = 0.002f;
+	
+	public bool itemBounceUp = false;
+	
+	void Start ()
+	{
+		StartCoroutine ("itemBounce", 0.8f);
+	}
+	
+	void Update ()
+	{
+		Vector3 myTransform = transform.position;
+		
+		if (itemBounceUp == true)
+		{
+			myTransform.y += bounceSpeed;
+			transform.position = myTransform;
+		}
+		
+		else if (itemBounceUp == false)
+		{
+			myTransform.y -= bounceSpeed;
+			transform.position = myTransform;
+		}
+	}
+	
+	IEnumerator itemBounce (float repeatAfter)
+	{
+		int i;
+		for (i=1; i>0; i++)
+		{
+			if (itemBounceUp)
+			{
+				itemBounceUp = false;
+				yield return new WaitForSeconds (repeatAfter);
+			}
+			else if (itemBounceUp == false)
+			{
+				itemBounceUp = true;
+				yield return new WaitForSeconds (repeatAfter);
+			}
+		}
+	}
+}
