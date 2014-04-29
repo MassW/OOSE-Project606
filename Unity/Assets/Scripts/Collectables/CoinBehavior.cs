0549c82899adc743e7944f886adaaddf09ed4272
Cecilie Warming <cwarming88@gmail.com>
2014-04-29 10:58:34 +0200

    copied changes from last bug fixing

--- Unity/Assets/Scripts/Collectables/CoinBehavior.cs
+++ Unity/Assets/Scripts/Collectables/CoinBehavior.cs
@@ -1,23 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoinBehavior : MonoBehaviour {
+public class CoinBehavior : MonoBehaviour
+{
 
 // controls the coinCounter goes one point up pr. coin collected
 // destroy coin when collisioned by the player
 
-	void Start(){
+    void OnCollisionEnter2D(Collision2D collider)
+    {
 
-		print("wuhu");
-	}
+        if (collider.transform.tag == "Player")
+        {
+            CoinController.coinCount++;
+            if (CoinController.coinCount == 10)
+            {
+                GameManager manager = Camera.main.GetComponent<GameManager>();
+                manager.won = true;
+            }
+            Destroy(this.gameObject);
+            print("hit");
+        }
 
-	void OnCollisionEnter2D(Collision2D collider){
+    }
 
-		if (collider.transform.tag == "Player") {
-			CoinController.coinCount++;
-			Destroy(this.gameObject);
-			print ("hit");
-		}
 
-	}
 }
--- Unity/Assets/Scripts/Collectables/CoinController.cs
+++ Unity/Assets/Scripts/Collectables/CoinController.cs
@@ -2,13 +2,13 @@
 using System.Collections;
 
 public class CoinController : MonoBehaviour {
-
-	// start coinCount at 0
-	public static int coinCount = 0;
-
-	// GUI interface
-	void OnGUI(){
-		string coinText = "Coins collected: " + coinCount;
-		GUI.Box(new Rect(Screen.width - 150, 20, 130, 20), coinText);
-	}
+    
+    // start coinCount at 0
+    public static int coinCount = 0;
+    
+    // GUI interface
+    void OnGUI(){
+        string coinText = "Coins collected: " + coinCount;
+        GUI.Box(new Rect(Screen.width - 150, 20, 130, 20), coinText);
+    }
 }
